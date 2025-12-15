e962a22a5d6f7f7cfc3a1d6f65070cfa092ce421
tonichevi <62634704+tonichevi@users.noreply.github.com>
2025-12-14 20:29:36 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import "./globals.css";
+
 
 /* -----------------------------------------------------
    GLOBAL BACKGROUND
